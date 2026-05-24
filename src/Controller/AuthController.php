f53db7479e215e11d4f9d5c8a36f38f3620ab5c2
Alejandro Gomez Anaya <alejandro.gomez@correounivalle.edu.co>
2026-05-23 19:38:13 -0500

    chore: remove debug error_log from googleConnect

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -39,8 +39,6 @@ final class AuthController extends AbstractController
 
         $redirectUri = rtrim($this->appBaseUrl, '/') . '/auth/google/callback';
 
-        error_log('DEBUG appBaseUrl=' . $this->appBaseUrl . ' redirectUri=' . $redirectUri);
-
         return $clientRegistry
             ->getClient('google')
             ->redirect(['openid', 'email', 'profile'], ['redirect_uri' => $redirectUri]);
